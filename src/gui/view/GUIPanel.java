fdeb66d528a8b3b74f6721dc7924656fdc62a175
PrestonStepp <31608773+PrestonStepp@users.noreply.github.com>
2017-11-13 08:39:33 -0700

    added helper methods

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -16,5 +16,30 @@ public class GUIPanel extends JPanel
 		this.appController = appController;
 		firstButton = new JButton("Wow a button");
 		
+		setupPanel();
+	}
+	
+	/**
+	 * Used to add all components to the subclass fo JPanel
+	 * This installs them into thepanel so they are seen in the GUI.
+	 */
+	private void setupPanel ()
+	{
+		this.add(firstButton);
+	}
+	
+	/**
+	 * This is used to hold all the layout and arrangement code foer SpringLayout.
+	 */
+	private void setupLayout()
+	{
+		
+	}
+	/**
+	 * This helper method is used to link any GUI components to the associated listeners.
+	 */
+	private void setupListeners()
+	{
+		
 	}
 }
