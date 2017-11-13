e7d287973f3b7727401ea50919b8618f0321a61e
PrestonStepp <31608773+PrestonStepp@users.noreply.github.com>
2017-11-13 08:04:49 -0700

    built frame class

--- src/gui/view/GUIFrame.java
+++ src/gui/view/GUIFrame.java
@@ -10,5 +10,15 @@ public class GUIFrame extends JFrame
 	public GUIFrame (GUIAppController appController)
 	{
 		super();
+		this.appController = appController;
+		
+		seturFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setTitle("the window title");
+		this.setSize(500, 500);
+		this.setVisible(true);
 	}
 }
