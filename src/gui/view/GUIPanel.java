293dcee4f523fde1553dfbf2c5917c6b2d1f1f12
PrestonStepp <31608773+PrestonStepp@users.noreply.github.com>
2017-11-13 08:20:15 -0700

    added constructor

---
+++ src/gui/view/GUIPanel.java
@@ -0,0 +1,17 @@
+package gui.view;
+
+import gui.controller.GUIAppController;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.Color;
+
+public class GUIPanel extends JPanel
+{
+	private GUIAppController appController;
+	private JButton firstButton;
+	
+	public GUIPanel(GUIAppController appConroller)
+	{
+		super();
+	}
+}
